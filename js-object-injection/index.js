85e2d1c36b567f0ef70a7df0297d51543e32a7f7
Cotonne <cotonne@users.noreply.github.com>
2019-11-04 08:29:20 +0100

    Make index.js recognized as vulnerable to object injection by eslint

--- js-object-injection/index.js
+++ js-object-injection/index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-var user = function() {
-	this.name = 'john';
+const user = function() {
+  this.name = 'john'; // eslint-disable-line no-invalid-this
 };
 
+/**
+ * Function vulnerable to object injection
+ * @param {any_string} input Dangerous value can be provided here
+ */
 function handler(input) {
-	console.log(input);
-	user['anyVal'] = user[input[0]](input[1]);
-	console.log(user);
+  console.log(input);
+  const v1 = input[0];
+  const v2 = user[v1];
+  user['anyVal'] = v2(input[1]);
+  console.log(user);
 }
 
-app.post("/api/user", (req, res) => res.send(handler(req.body)));
+app.post('/api/user', (req, res) => res.send(handler(req.body)));
 
 app.listen(3000, '127.0.0.1');
